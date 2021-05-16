28c4851ea72f1cf62322b53edba22a4ec7964d95
bharanidharan1999 <bharani472tnj@gmail.com>
2021-05-16 22:07:21 +0530

    commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyApWk1waYyPup44o_pljY7Kbu2nXqwhOGY",
+    authDomain: "c-g-blog.firebaseapp.com",
+    projectId: "c-g-blog",
+    storageBucket: "c-g-blog.appspot.com",
+    messagingSenderId: "499397596851",
+    appId: "1:499397596851:web:cd38079f344be3108402c6",
+    measurementId: "G-6BQMEGMPFV"
+};
